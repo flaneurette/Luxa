c78fa68a59c994b535f6045a5c347eb5a5dc54f2
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2024-07-05 20:21:56 +0200

    Add files via upload

---
+++ demo/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+  <title>UX HTTP</title>
+<script src="UX.js"></script>
+</head>
+<body>
+
+<div id="app">
+<h1>UX DEMO</h1>
+ <p>{{blogTitle}}</p>
+  <div>
+    <div :loop="blogs">
+	<div>
+		 <h1>{{snippet_title}}</h1>
+		 <p>{{snippet_text}}</p>
+	 </div>
+    </ul>
+  </div>
+</div>
+	
+	<script>
+
+	let app = new UX();
+	
+	app.http('../API.php', 'callback', mycall);
+	
+	function mycall(dataset) {
+		app.load({
+			data: {
+				blogTitle: 'HTTP test',
+				blogs: JSON.parse(dataset)
+			}
+		});
+	}
+	</script>
+</body>
+</html>
